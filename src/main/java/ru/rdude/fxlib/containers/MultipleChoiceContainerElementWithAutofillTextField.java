07eb59677837bc784c06b664347573c0e388ec79
TheRussianDude <hritnik@yandex.ru>
2020-11-06 00:17:16 +0300

    added autocomplition container element extended options setter

--- src/main/java/ru/rdude/fxlib/containers/MultipleChoiceContainerElementWithAutofillTextField.java
+++ src/main/java/ru/rdude/fxlib/containers/MultipleChoiceContainerElementWithAutofillTextField.java
@@ -4,6 +4,8 @@ import ru.rdude.fxlib.textfields.AutocomplitionTextField;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MultipleChoiceContainerElementWithAutofillTextField<T> extends MultipleChoiceContainerElementWithTextField<T> {
 
@@ -17,6 +19,18 @@ public class MultipleChoiceContainerElementWithAutofillTextField<T> extends Mult
     }
 
     public void setAutocomplitionElements(Collection<String> collection) {
+        ((AutocomplitionTextField) textField).setElements(collection);
+    }
+
+    @Override
+    public void setExtendedOptions(Object... options) {
+        Set<String> collection = new HashSet<>();
+        for (Object option : options) {
+            if (!(option instanceof Collection)) {
+                throw new IllegalArgumentException("Extended option for this container element type must be instance of collection");
+            }
+            collection.addAll((Collection<? extends String>) option);
+        }
         ((AutocomplitionTextField) textField).setElements(collection);
     }
 }
